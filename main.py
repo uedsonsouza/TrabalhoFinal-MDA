6a7055407fa1075e24dfa04369cb1d93a3b3d384
Mateus Moreira <mateusmduraes@hotmail.com>
2022-06-21 23:10:41 -0300

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+##################################################################|TRABALHO FINAL - OTIMIZAÇÃO NÃO LINEAR|#########################################################################################
+# Universidade Estadual de Montes claros - UNIMONTES
+# Curso de Engenharia de Sistema - Otimização Não Linear - 5° Período
+# Professor - Lenir De Abreu Júnior
+#
+# Equipe 01 — Métodos de Direção de Busca - Direções Aleatória
+#   == GRUPO ==
+#   -> Mateus Moreira Durães
+#   -> 
+#   -> 
+#   -> 
+###################################################################################################################################################################################################
+from os import system
+
+# Roda o programa
+if __name__ == '__main__':
+    system('cls')
